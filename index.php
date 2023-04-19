f35d56ab2b43a1561ed4a8dcf8beb3361b925d09
MarcoCocco <marcococco28@gmail.com>
2023-04-19 16:02:13 +0200

    feat: creato form e funzione per generare la password

--- index.php
+++ index.php
@@ -12,7 +12,29 @@ Verificato il corretto funzionamento del nostro codice, spostiamo la logica in u
 Milestone 3 (BONUS)
 Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà la password da mostrare all’utente. -->
 
+<?php
+//Creo la variabile presa dal form
+$numberOfCharacters = $_GET['numberCharacters'] ?? 0;
 
+//Funzione che, in base al valore dato nel form, preleva una serie di caratteri dall'array "$characters" tramite un ciclo for li pusha all'interno dell'array vuoto "$password".
+function getRandomPassword($numberOfCharacters)
+{
+    $characters = ['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '?', '!', '-', '_'];
+
+    $password = [];
+
+    for ($i = 0; $i < $numberOfCharacters; $i++) {
+        //Variabile che rappresenta l'elemento random prelevato dall'array "$characters" grazie alla funzione "array_rand"
+        $randomElement = $characters[array_rand($characters)];
+        array_push($password, $randomElement);
+    }
+
+    //Tramite la funzione "implode()", trasformo l'array "$password" in una stringa composta dal suo contenuto
+    $stringPassword = implode($password);
+
+    return $stringPassword;
+}
+?>
 
 <!DOCTYPE html>
 <html lang="it">
@@ -25,11 +47,13 @@ Invece di visualizzare la password nella index, effettuare un redirect ad una pa
 </head>
 
 <body>
-<form action="index.php" method="GET">
-    <label for="numberCharacters">Quanti caratteri deve avere la tua Password?</label><br>
-    <input type="number" name="numberCharacters" id="numberCharacters">
-    <button type="submit">Genera</button>
-</form>
+    <form action="index.php" method="GET">
+        <label for="numberCharacters">Quanti caratteri deve avere la tua Password?</label><br>
+        <input type="number" name="numberCharacters" id="numberCharacters">
+        <button type="submit">Genera</button>
+    </form>
+
+    <p>La tua password è: <?php echo getRandomPassword($numberOfCharacters) ?></p>
 </body>
 
 </html>
