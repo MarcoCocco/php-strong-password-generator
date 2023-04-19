dd52320695ec19e4b17041caf4a055a702924503
MarcoCocco <marcococco28@gmail.com>
2023-04-19 14:40:03 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!-- Descrizione
+Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure.
+L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
+
+Milestone 1
+Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+Scriviamo tutto (logica e layout) in un unico file index.php
+
+Milestone 2
+Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale
+
+Milestone 3 (BONUS)
+Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà la password da mostrare all’utente. -->
+
+
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Strong Password Generator</title>
+</head>
+
+<body>
+<form action="index.php" method="GET">
+    <label for="numberCharacters">Quanti caratteri deve avere la tua Password?</label><br>
+    <input type="number" name="numberCharacters" id="numberCharacters">
+    <button type="submit">Genera</button>
+</form>
+</body>
+
+</html>
